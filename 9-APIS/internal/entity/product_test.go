84ec1422d1accd512c93254a959a479c19d0e676
Joao Lino <linusmannuel@gmail.com>
2026-06-22 07:35:46 +0100

    feat: testing product entity

---
+++ 9-APIS/internal/entity/product_test.go
@@ -0,0 +1,41 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProduct(t *testing.T) {
+	p, err := NewProduct("Product 1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ID)
+	assert.Equal(t, "Product 1", p.Name)
+	assert.Equal(t, 10, p.Price)
+}
+
+func TestProductWhereNameIsRequired(t *testing.T) {
+	p, err := NewProduct("", 10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+	p, err := NewProduct("Product 1", -12)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+	p, err := NewProduct("Product 1", 0)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestProductValidate(t *testing.T) {
+	p, err := NewProduct("Product 1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.Nil(t, p.Validate())
+}
