983c814713dff09dc9a4d5fd42e265e28208a415
Joao Lino <linusmannuel@gmail.com>
2026-06-23 09:26:12 +0100

    feat: add testFindAllProducts

--- 9-APIS/internal/infra/database/product_db_test.go
+++ 9-APIS/internal/infra/database/product_db_test.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"math/rand/v2"
 	"testing"
+
+	"fmt"
 
 	"github.com/glebarez/sqlite"
 	"github.com/linusmanuel/backend-engineering/goexpert/9-APIS/internal/entity"
@@ -21,4 +24,35 @@ func TestCreateNewProduct(t *testing.T) {
 	err = productDB.Create(product)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, product.ID)
+}
+
+func TestFindAllProducts(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	for i := 1; i < 24; i++ {
+		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
+		assert.NoError(t, err)
+		db.Create(product)
+	}
+	productDB := NewProduct(db)
+	products, err := productDB.FindAll(1, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 1", products[0].Name)
+	assert.Equal(t, "Product 10", products[9].Name)
+
+	products, err = productDB.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 11", products[0].Name)
+	assert.Equal(t, "Product 20", products[9].Name)
+
+	products, err = productDB.FindAll(3, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 3)
+	assert.Equal(t, "Product 21", products[0].Name)
+	assert.Equal(t, "Product 23", products[2].Name)
 }
