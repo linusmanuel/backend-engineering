5f29605dc5b422c305fdd2470cc58b00d4d3b29d
Joao Lino <linusmannuel@gmail.com>
2026-06-23 10:00:03 +0100

    feat: update find all test

--- 9-APIS/internal/product_test.go
+++ 9-APIS/internal/product_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.00, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 10)
+	p, err := NewProduct("", 10.00)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
@@ -34,7 +34,7 @@ func TestProductWhenPriceIsInvalid(t *testing.T) {
 }
 
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
